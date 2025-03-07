37a460cd264d963f53806c86137ad7e9a359e8b9
Hussnain <howrex95@gmail.com>
2025-03-07 16:39:13 +0500

    sxa

--- resources/views/sitemaps/sitemap_index.blade.php
+++ resources/views/sitemaps/sitemap_index.blade.php
@@ -1,37 +1,37 @@
-@php echo '<?xml version="1.0" encoding="UTF-8"@endphp'; ?>
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <sitemap>
-        <loc>https://examprince.com/static/sitemap.xml</loc>
-        <lastmod>2025-01-24</lastmod>
-    </sitemap>
+  <sitemap>
+    <loc>https://examprince.com/static/sitemap.xml</loc>
+    <lastmod>2025-01-24</lastmod>
+  </sitemap>
 
-    @foreach ($vendorPages as $vendor)
-        <sitemap>
-            <loc>{{ $vendor['loc'] }}</loc>
-            <lastmod>{{ $vendor['lastmod'] }}</lastmod>
-        </sitemap>
-    @endforeach
-
-    @foreach ($examPages as $exam)
-        <sitemap>
-            <loc>{{ $exam['loc'] }}</loc>
-            <lastmod>{{ $exam['lastmod'] }}</lastmod>
-        </sitemap>
-    @endforeach
-    @foreach ($certificatePages as $cert)
-        <sitemap>
-            <loc>{{ $cert['loc'] }}</loc>
-            <lastmod>{{ $cert['lastmod'] }}</lastmod>
-        </sitemap>
-    @endforeach
-    @foreach ($trainingCoursePages as $trainingCourse)
-        <sitemap>
-            <loc>{{ $trainingCourse['loc'] }}</loc>
-            <lastmod>{{ $trainingCourse['lastmod'] }}</lastmod>
-        </sitemap>
-    @endforeach
+  @foreach ($vendorPages as $vendor)
     <sitemap>
-        <loc>https://examprince.com/blogs-1/sitemap.xml</loc>
-        <lastmod>2025-01-24</lastmod>
+      <loc>{{ $vendor['loc'] }}</loc>
+      <lastmod>{{ $vendor['lastmod'] }}</lastmod>
     </sitemap>
+  @endforeach
+
+  @foreach ($examPages as $exam)
+    <sitemap>
+      <loc>{{ $exam['loc'] }}</loc>
+      <lastmod>{{ $exam['lastmod'] }}</lastmod>
+    </sitemap>
+  @endforeach
+  @foreach ($certificatePages as $cert)
+  <sitemap>
+    <loc>{{ $cert['loc'] }}</loc>
+    <lastmod>{{ $cert['lastmod'] }}</lastmod>
+  </sitemap>
+@endforeach
+@foreach ($trainingCoursePages as $trainingCourse)
+<sitemap>
+  <loc>{{ $trainingCourse['loc'] }}</loc>
+  <lastmod>{{ $trainingCourse['lastmod'] }}</lastmod>
+</sitemap>
+@endforeach
+  <sitemap>
+    <loc>https://examprince.com/blogs-1/sitemap.xml</loc>
+    <lastmod>2025-01-24</lastmod>
+  </sitemap>
 </sitemapindex>
